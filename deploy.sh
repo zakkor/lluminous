1e8dc59f41b2d6974d5dffe75e4523a0dbf8bade
Zakkor <edward.partenie@gmail.com>
2024-04-15 21:32:22 +0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run build && rsync -r -avz -e ssh ~/src/chat/build/* root@137.184.193.108:/var/www/html/lluminous.chat
+npm run build && rsync -r -avz -e ssh ./dist/* root@137.184.193.108:/var/www/html/lluminous.chat
