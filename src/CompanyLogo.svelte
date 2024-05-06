e4e22f41e4846013fdba6acbc3b0800bef9fa060
Zakkor <edward.partenie@gmail.com>
2024-05-06 15:46:57 +0300

    Update logos

--- src/CompanyLogo.svelte
+++ src/CompanyLogo.svelte
@@ -4,27 +4,16 @@
 
 {#if model && model.provider}
 	{#if model.id.startsWith('openai')}
-		<img
-			src="https://www.svgrepo.com/show/306500/openai.svg"
-			loading="lazy"
-			class="h-4 w-4 rounded"
-			alt=""
-		/>
+		<img src="/logos/openai.ico" loading="lazy" class="h-3.5 w-3.5 rounded-sm" alt="" />
 	{:else if model.id.startsWith('anthropic')}
-		<img
-			src="https://asset.brandfetch.io/idmJWF3N06/ida_TmGBgC.jpeg"
-			loading="lazy"
-			class="h-4 w-4 rounded"
-			alt=""
-		/>
+		<img src="/logos/anthropic.jpeg" loading="lazy" class="h-3.5 w-3.5 rounded-sm" alt="" />
 	{:else if model.id.startsWith('meta-llama')}
-		<img src="/logos/meta.png" loading="lazy" class="h-3.5 w-3.5 rounded" alt="" />
+		<img src="/logos/meta.png" loading="lazy" class="h-3.5 w-3.5" alt="" />
+	{:else if model.id.startsWith('mistralai')}
+		<img src="/logos/mistral.png" loading="lazy" class="h-3.5 w-3.5" alt="" />
+	{:else if model.id.startsWith('cohere')}
+		<img src="/logos/cohere.png" loading="lazy" class="h-3.5 w-3.5" alt="" />
 	{:else if model.provider === 'Groq'}
-		<img
-			src="https://asset.brandfetch.io/idxygbEPCQ/idzCyF-I44.png"
-			loading="lazy"
-			class="h-3.5 w-3.5 rounded"
-			alt=""
-		/>
+		<img src="/logos/groq.png" loading="lazy" class="h-3.5 w-3.5 rounded-sm" alt="" />
 	{/if}
 {/if}
