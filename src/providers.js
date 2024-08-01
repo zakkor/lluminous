ba66e06edb0a37e23f5ad3ec8c94a254b3ec945e
Zakkor <edward.partenie@gmail.com>
2024-08-01 16:39:15 +0300

    Update Groq models

--- src/providers.js
+++ src/providers.js
@@ -119,7 +119,7 @@ export const priorityOrder = [
 		],
 	},
 	{
-		exactly: ['llama3-70b-8192', 'llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma2-9b-it'],
+		exactly: ['llama-3.1-70b-versatile', 'llama-3.1-8b-instant'],
 	},
 	{
 		exactly: [
